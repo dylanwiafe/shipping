515718b43bbc3db28d2580d2dc98bfff3b054ee2
Dylan Wiafe <dylanwiafe@gmail.com>
2021-08-27 17:12:51 -0400

    establishing connection

--- index.js
+++ index.js
@@ -2,7 +2,32 @@ import express from "express";
 const app = express();
 import mongoose from "mongoose";
 //bookings routers
+import "dotenv/config";
 
 app.listen(8081, () => {
   console.log("the app is listening");
 });
+const uri = process.env.ATLAS_URI;
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then((res) => {
+    console.log("you have connected to the database");
+  })
+  .catch((err) => {
+    console.log(Error, err.message);
+  });
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log(
+    "a connection with your mongodb atlas database has been established"
+  );
+});
+
+app.use(express.json());
---
+++ src/routes/bookings.js
@@ -0,0 +1,10 @@
+import express from "express";
+const bookingRouter = express.Router();
+
+//getAllbookings{
+// declare our booking object
+// do a find on our Data
+// return it in json format
+// }
+
+module.exports(bookingRouter);
